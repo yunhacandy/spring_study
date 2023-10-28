101f670f4e54100e893dbe077032f1ea219a0f5d
yunhacandy <yunhacandy@g.hongik.ac.kr>
2023-10-28 22:27:48 +0900

    섹션7

--- core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -1,4 +1,12 @@
 package hello.core.annotation;
 
-public interface MainDiscountPolicy {
+import org.springframework.beans.factory.annotation.Qualifier;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER,
+        ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
 }
