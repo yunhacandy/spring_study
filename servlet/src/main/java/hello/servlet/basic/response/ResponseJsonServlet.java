1a625211b64122608323e6ebec869f3369a56d71
yunhacandy <yunhacandy@g.hongik.ac.kr>
2023-11-03 01:58:10 +0900

    섹션2

--- servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -1,27 +1,20 @@
 package hello.servlet.basic.response;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hello.servlet.basic.HelloData;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-/**
- * http://localhost:8080/response-json
- *
- */
-@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
-public class ResponseJsonServlet {
+public class ResponseJsonServlet extends HttpServlet {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         //Content-Type: application/json
         response.setHeader("content-type", "application/json");
